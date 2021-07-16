79b9361e467ca199c5d2e3885ced063e741992a2
sravansivadas <73734099+sravansivadas@users.noreply.github.com>
2021-07-17 04:43:24 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,11 @@ class Contact extends Component {
   render() {
     return (
       <div classname="linkclass">
-        <p><b>You Get in Touch Through Social or Call</b></p>
+        <p><b>You Get in Touch Through Social or Call</b></p><br></br>
+
+         <p>Name: Sravan Sivadas</p>
+         <p>ID No. : 20181CSE0703</p><br></br>
+
         <span style={{cursor: "pointer"}}>
         <a id="link1" href="https://wa.me/+918892341964"><br></br><SiWhatsapp color="#25D366"size="2em"/> <b>Whatsapp</b></a>
         <br></br>
